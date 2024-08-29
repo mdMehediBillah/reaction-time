ccb20452b9779d1ca11ab08c15936b5a0c960877
mdMehediBillah <mailbillah@gmail.com>
2024-08-29 17:13:34 +0200

    Reaction time and restart button function are working

---
+++ vite-project/src/components/Block.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="block" v-if="showBloack" @click="stopTimer">
+    <h3>Click me</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBloack: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    console.log("mounted");
+    setTimeout(() => {
+      this.showBloack = true;
+      // console.log(this.delay);
+      this.startTimer();
+    }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.showBloack = true;
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      console.log(this.reactionTime);
+      this.$emit("end", this.reactionTime);
+    },
+
+    // handleClick() {
+    //   this.showBloack = false;
+    //   this.$emit("block-clicked");
+    // },
+  },
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  background-color: rgb(10, 92, 125);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+  color: white;
+  border-radius: 1.4rem;
+  padding: 8rem 0;
+  margin: 40px auto;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  cursor: pointer;
+}
+</style>
