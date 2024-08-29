2190c76aaf3913f5a0591f6204a4fb695a63d585
mdMehediBillah <mailbillah@gmail.com>
2024-08-29 14:47:27 +0200

    initial commit

---
+++ vite-project/src/App.vue
@@ -0,0 +1,70 @@
+<script>
+import Modal from "./components/Modal.vue";
+import { ref } from "vue";
+// Reactive reference ref Composition API
+// const msg = ref("Hello Vite");
+export default {
+  name: "App",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: "Hello Vite",
+      // header: "This is a header from data",
+      // text: "Grab the content from data",
+      // theme: "light",
+      showModal: false,
+      showModal2: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModal2() {
+      this.showModal2 = !this.showModal2;
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <div v-if="showModal">
+      <!-- slot -->
+      <Modal theme="sale" @close="toggleModal">
+        <h1>Mehedi using slot</h1>
+        <p>Content for madal</p>
+        <template v-slot:link>
+          <a href="https://www.google.com">Google</a>
+        </template>
+      </Modal>
+      <!-- <Modal :header="header" :text="text" theme="sale" @close="toggleModal" /> -->
+    </div>
+    <div v-if="showModal2">
+      <!-- slot -->
+      <Modal theme="salee" @close="toggleModal2">
+        <h1 class="headerTitle">Second Modal using click evet</h1>
+        <p>Content for madal</p>
+        <template v-slot:link>
+          <a href="https://www.google.com">Google</a>
+        </template>
+      </Modal>
+      <!-- <Modal :header="header" :text="text" theme="sale" @close="toggleModal" /> -->
+    </div>
+    <button @click.alt="toggleModal">Toggle Modal(alt+click)</button>
+    <button @click="toggleModal2">Second Modal</button>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: rgb(27, 134, 95);
+  font-size: 2rem;
+}
+p {
+  color: rgb(51, 51, 51);
+}
+</style>
---
+++ vite-project/src/components/Modal.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  props: ["header", "text", "theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<!-- template -->
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <slot>default Content</slot>
+      <div>
+        <slot name="link"></slot>
+      </div>
+      <!-- <h1>{{ header }}</h1>
+      <p>{{ text }}</p> -->
+    </div>
+  </div>
+</template>
+
+<!-- style -->
+<style scoped>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+h1 {
+  color: rgb(36, 36, 121);
+  font-size: 2rem;
+}
+p {
+  color: rgb(51, 51, 51);
+}
+.sale {
+  background: cyan;
+}
+</style>
